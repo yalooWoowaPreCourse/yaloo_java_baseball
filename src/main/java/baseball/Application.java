b7e9e61d06c7ecf91200dad524c5ef6b04fdc948
hwiju yeom <yeom456@gmail.com>
2023-10-25 10:45:18 +0900

    Update Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,10 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        baseball.model.BaseballGameModel model = new baseball.model.BaseballGameModel();
+        baseball.view.BaseballGameView view = new baseball.view.BaseballGameView();
+        baseball.controller.BaseballGameController controller = new baseball.controller.BaseballGameController(model, view);
+
+        controller.startGame();
     }
 }
